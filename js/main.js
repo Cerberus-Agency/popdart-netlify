43e5d3cecfb4b5edfc344db028cd5266e9396ef6
David Rhoden <david@davidrhoden.com>
2020-03-15 05:29:35 -0500

    change slickslider to 4 at breakpoint 1024

--- js/main.js
+++ js/main.js
@@ -149,8 +149,8 @@ $(document).ready(function(){
 	{
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToShow: 4,
+        slidesToScroll: 4
       }
     },
     {
