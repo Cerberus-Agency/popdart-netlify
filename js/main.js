9cbc7ba59e79c81df4b5d379707dde5f2ed1a45c
David Rhoden <david@davidrhoden.com>
2020-03-17 09:25:28 -0500

    more debug on the inspiration slider

--- js/main.js
+++ js/main.js
@@ -177,13 +177,13 @@ $(document).ready(function(){
     autoplay: false,
     variableWidth: true,
     responsive: [
-	// {
- //      breakpoint: 1024,
- //      settings: {
- //        slidesToShow: 4,
- //        slidesToScroll: 4
- //      }
- //    },
+	{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
     {
       breakpoint: 768,
       settings: {
